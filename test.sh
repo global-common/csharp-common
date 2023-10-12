2ddcaea51ab16b4a96afb804ec89585ec6e8e99e
JavaCommons Technologies <javacommons@gmail.com>
2023-10-12 12:21:27 +0900

    .

--- test.sh
+++ test.sh
@@ -4,5 +4,4 @@ set -e
 #deno compile --allow-all my7z.js
 #popd
 #my7z.exe x -aos -otmp https://cdn.azul.com/zulu/bin/zulu17.44.53-ca-jdk17.0.8.1-win_x64.zip
-wget -nc -O tmp.zip https://cdn.azul.com/zulu/bin/zulu17.44.53-ca-jdk17.0.8.1-win_x64.zip || true
-my7z.exe x -aos -otmp tmp.zip
+7z.exe x -aos -otmp zulu17.44.53-ca-jdk17.0.8.1-win_x64-stripped.zip
